b3741fc16905034422e610123030a8ef2aa8c9ba
darlissomls <darlissomls@gmail.com>
2024-07-25 01:55:01 -0300

    Sample use REGISTER_COMPONENT

--- main.cpp
+++ main.cpp
@@ -10,41 +10,19 @@ class Render : public Component {
 class Math : public Component {
 };
 
+REGISTER_COMPONENT(Math)
+
 int main() {
     std::cout << "Entity: 0" << std::endl;
     auto& entity0 = Entity::Create();
 
-    auto& transform = *static_cast<Transform*>(entity0.addComponent("9Transform"));
-    std::cout << "Original: " << transform.id << std::endl;
-    transform.id = 10;
-    std::cout << "Modify: " << transform.id << std::endl;
+    entity0.addComponent("4Math");
 
-    if (entity0.hasComponent<Transform>()) {
-        std::cout << "Has transform componenet, using template." << std::endl;
-
-        auto& tt = entity0.getComponent<Transform>();
-
-        std::cout << "Last: " << tt.id << std::endl;
-        tt.id = 800;
-        std::cout << "Befor: " << tt.id << std::endl;
+    if (entity0.hasComponent("4Math")) {
+        std::cout << "Has component." << std::endl;
     } else {
-        std::cout << "Component transform do not exists, using template." << std::endl;
+        std::cout << "do not has component." << std::endl;
     }
-
-    if (entity0.hasComponent("9Transform")) {
-        std::cout << "Has transform componenet, using string." << std::endl;
-
-        auto& ts = *static_cast<Transform*>(entity0.getComponent("9Transform"));
-
-        std::cout << "Last: " << ts.id << std::endl;
-        ts.id = 40;
-        std::cout << "Befor: " << ts.id << std::endl;
-    } else {
-        std::cout << "Component transform do not exists, using string." << std::endl;
-    }
-
-    std::cout << "Finished: " << transform.id << std::endl;
-
     return 0;
 }
 
